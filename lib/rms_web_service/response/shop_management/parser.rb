8f404dd8d40f623f0072b16525da584d7ab0374f
yuhei mukoyama <yuhei.mukoyama@gmail.com>
2018-06-21 19:12:31 +0900

    :bug: fixtypo system status parser

--- lib/rms_web_service/response/shop_management/parser.rb
+++ lib/rms_web_service/response/shop_management/parser.rb
@@ -10,7 +10,7 @@ module RmsWebService
         def initialize(xml)
           @raw_xml = xml.is_a?(::File) ? xml.read : xml
           @parsed_xml = Nokogiri::XML.parse(@raw_xml)
-          @system_status = Cabinet::Status.new(@raw_xml)
+          @system_status = ShopManagement::Status.new(@raw_xml)
           @result_code = @parsed_xml.xpath("//resultCode").first.content if @parsed_xml.xpath("//resultCode").present?
           @errors = []
         end
